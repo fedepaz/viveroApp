7b35cc3a835f205a756744ff237c42fe3c57485b
fedepaz89 <fedepaz89@gmail.com>
2025-09-19 21:28:05 -0300

    chore: frenar el .next re culiado

--- apps/frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\n  \"app\": {\n    \"[project]/apps/frontend/src/app/[locale]/page\": [\n      \"static/media/Geist_Variable-s.p.f19e4721.woff2\",\n      \"static/media/GeistMono_Variable.p.73882635.woff2\"\n    ]\n  },\n  \"appUsingSizeAdjust\": true,\n  \"pages\": {},\n  \"pagesUsingSizeAdjust\": false\n}"
